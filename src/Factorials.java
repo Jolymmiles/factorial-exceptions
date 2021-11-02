503f2c7ef6e21338f05c6d180f14a06664aeadbe
Валентин <valmak04@gmail.com>
2021-11-02 18:13:08 +0400

    Переместил метод и исключение

--- src/Factorials.java
+++ src/Factorials.java
@@ -9,27 +9,19 @@ public class Factorials {
         while (keepGoing.equals("Y")) {
             System.out.print("Введите число: ");
             int val = scan.nextInt();
-            if (val <0 | val > 15) {
-                try {
-                    throw new IllegalArgumentException();
-                } catch (IllegalArgumentException e) {
-                    if (val < 0) {
-                        System.out.println("Факториала для отрацтельного числа нету");
-                    } else if (val > 15) {
-                        System.out.println("Факториала е помещается в int");
-                    }
-
-                    break;
-                }
-            }
+            try {
                 System.out.println(
                         String.format(
                                 "Факториал(\"%d\") = %d",
                                 val, MathUtils.factorial(val)
                         )
                 );
-                System.out.print("Вычислить другой факториал? (y/n) ");
-                keepGoing = scan.next().toUpperCase();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.print("Вычислить другой факториал? (y/n) ");
+            keepGoing = scan.next().toUpperCase();
 
         }
     }
--- src/MathUtils.java
+++ src/MathUtils.java
@@ -1,9 +1,13 @@
 public class MathUtils {
     public static int factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Факториал отриацтельного числа не сущеастует");
+        } else if (n > 15) {
+            throw new IllegalArgumentException("Факториал не поместился");
+        }
         int fac = 1;
         for (int i = n; i > 0; i--)
             fac *= i;
-
         return fac;
     }
 }
