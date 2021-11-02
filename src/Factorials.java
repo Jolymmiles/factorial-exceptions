e118d9e5be27ed0380f2fb7c7ad4a1490d63d12d
Валентин <valmak04@gmail.com>
2021-11-02 17:52:28 +0400

    Done

--- src/Factorials.java
+++ src/Factorials.java
@@ -9,14 +9,28 @@ public class Factorials {
         while (keepGoing.equals("Y")) {
             System.out.print("Введите число: ");
             int val = scan.nextInt();
-            System.out.println(
-                    String.format(
-                            "Факториал(\"%d\") = %d",
-                            val, MathUtils.factorial(val)
-                    )
-            );
-            System.out.print("Вычислить другой факториал? (y/n) ");
-            keepGoing = scan.next().toUpperCase();
+            if (val <0 | val > 15) {
+                try {
+                    throw new IllegalArgumentException();
+                } catch (IllegalArgumentException e) {
+                    if (val < 0) {
+                        System.out.println("Факториала для отрацтельного числа нету");
+                    } else if (val > 15) {
+                        System.out.println("Факториала е помещается в int");
+                    }
+
+                    break;
+                }
+            }
+                System.out.println(
+                        String.format(
+                                "Факториал(\"%d\") = %d",
+                                val, MathUtils.factorial(val)
+                        )
+                );
+                System.out.print("Вычислить другой факториал? (y/n) ");
+                keepGoing = scan.next().toUpperCase();
+
         }
     }
 }
